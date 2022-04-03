f5c3c5f3cd88795add588c7171f33d2ff47fb4f5
rohulaminwd <rohulaminwd@gmail.com>
2022-04-04 00:38:38 +0600

    some change and added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './component/Navbar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1>Hello</h1>
     </div>
   );
---
+++ src/component/Navbar/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <h1>this is nav bar</h1>
+        </div>
+    );
+};
+
+export default NavBar;
