fd194d84277cebb38910b9d0c776ecf9918a6ff3
rohulaminwd <rohulaminwd@gmail.com>
2022-04-05 00:27:25 +0600

    rating added

--- src/component/Home/StudentReview.js
+++ src/component/Home/StudentReview.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import "./home.css"
+import { FaStar } from 'react-icons/fa';
 
 const StudentReview = ({review}) => {
     const {name, img, title, ratings} = review;
+    
     return (
         <div className='p-2 rounded-3 border'>
             <div className="">
@@ -10,7 +12,7 @@ const StudentReview = ({review}) => {
             </div>
             <div className="text-start my-2">
                 <h3 className='fw-bold'>{name}</h3>
-                <h6>Rating: {ratings}</h6>
+                <h6>Rating:  <span className='fw-bold text-danger'>{ratings.map(i => <FaStar />)}</span></h6>
                 <p>{title}</p>
             </div>
         </div>
